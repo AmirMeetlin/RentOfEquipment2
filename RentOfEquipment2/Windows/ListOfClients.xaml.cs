c65c8cf623575814bf14ca304eb5f0067c57d364
Amir <metlin.amir@mail.ru>
2022-01-27 14:20:34 +0300

    Добавьте файлы проекта.

---
+++ RentOfEquipment2/Windows/ListOfClients.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RentOfEquipment2.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ListOfClients.xaml
+    /// </summary>
+    public partial class ListOfClients : Window
+    {
+        public ListOfClients()
+        {
+            InitializeComponent();
+        }
+    }
+}
